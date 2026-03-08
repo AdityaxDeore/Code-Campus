5ff16670c3a33e2356752e5de6269528d31150fc
AdityaxDeore <AdityaxDeore@users.noreply.github.com>
2026-03-08 04:47:22 +0000

    deploy: 50d0490e8307b2fdf4df2ca8c67b7721dab8ed8d

--- assets/useAuth-Cu5QGV6f.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./vendor-react-BNeFDwYW.js";import{o as i}from"./index-NdL-ZZP6.js";const p=()=>{const[e,r]=t.useState(null),[o,u]=t.useState(!0),[n,a]=t.useState(null);return t.useEffect(()=>{const s=i(c=>{r(c),u(!1),a(null)});return()=>{typeof s=="function"&&s()}},[]),{user:e,loading:o,error:n,isAuthenticated:!!e}};export{p as u};
-//# sourceMappingURL=useAuth-Cu5QGV6f.js.map
