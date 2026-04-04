1889c060f765475587c771f9790d3202edc9a03b
ved-jadhav-10 <ved-jadhav-10@users.noreply.github.com>
2026-04-04 12:10:21 +0000

    deploy: 007d99764b3eea66cd9c9e494ecb1255e887fa80

--- assets/useAuth-TtBw6T8T.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"./vendor-react-DQHhJqIw.js";import{o as i}from"./index-X5j_foTO.js";const p=()=>{const[e,r]=t.useState(null),[o,u]=t.useState(!0),[n,a]=t.useState(null);return t.useEffect(()=>{const s=i(c=>{r(c),u(!1),a(null)});return()=>{typeof s=="function"&&s()}},[]),{user:e,loading:o,error:n,isAuthenticated:!!e}};export{p as u};
-//# sourceMappingURL=useAuth-TtBw6T8T.js.map
